7fc202e1848397cf853aa855cf20a93c98850e7a
Jain4556 <sunil925business@gmail.com>
2026-02-27 15:24:49 +0530

    fix_middleware_error

--- proxy.ts
+++ proxy.ts
@@ -17,45 +17,37 @@ export default clerkMiddleware(async (auth, req) => {
   }
 
   if (userId) {
-    const client = await clerkClient(); // 
+    const client = await clerkClient();
     const user = await client.users.getUser(userId);
-
     const role = user.publicMetadata.role as string | undefined;
 
-    // admin role redirection
+    // Admin redirect from home
     if (role === "admin" && req.nextUrl.pathname === "/") {
       return NextResponse.redirect(new URL("/admin", req.url));
     }
 
-
-    // prevent non admin user to access the admin routes
-    if(role !== "admin" && req.nextUrl.pathname.startsWith("/admin")){
-      return NextResponse.redirect(new URL("/dashboard", req.url ))
+    // Prevent non-admin from accessing admin routes
+    if (role !== "admin" && req.nextUrl.pathname.startsWith("/admin")) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
     }
 
-    //  redirect auth user trying to access pulbic routes
-    if(isPublicRoute.includes(req.nextUrl.pathname))
+    // Redirect logged-in users away from public routes
+    if (isPublicRoute(req)) {
       return NextResponse.redirect(
-      new URL(
-        role === "admin" ? "/admin/dashboard" : "/dashboard",
-        req.url
-      )
-    )
+        new URL(
+          role === "admin" ? "/admin/dashboard" : "/dashboard",
+          req.url
+        )
+      );
+    }
   }
-  
-
-
 
   return NextResponse.next();
 });
 
-
-
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+};
